e80a92dbde8cdc9084c006abc6676e84763f770c
Felipe Bezerra <felipeangells95@gmail.com>
2017-01-22 20:15:27 -0200

    hashing the password for more security

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,8 +1,25 @@
 var jwt = require('jsonwebtoken');
+var bcrypt = require('bcryptjs');
 
-var data = {
-	id: 10
-};
+var password = '123abc!';
 
-var token = jwt.sign(data, '123abc');
-var decoded = jwt.verify(token, '123abc');
+bcrypt.genSalt(10, (err, salt) => {
+	bcrypt.hash(password, salt, (err, hash) =>{
+		
+	});
+});
+var pass = '$2a$10$RhWJC2B9vPb0GCqrEaeWhOKHo8gzjrkQRTu98rqLfQFsC9I7.rkyG';
+bcrypt.compare(password, pass, (err, res) =>{
+	console.log(res);
+})
+
+
+
+
+
+// var data = {
+// 	id: 10
+// };
+
+// var token = jwt.sign(data, '123abc');
+// var decoded = jwt.verify(token, '123abc');
