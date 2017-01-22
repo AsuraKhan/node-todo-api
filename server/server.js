3afdf9c8e3b16e88643c548d767646470d16e72d
Felipe Bezerra <felipeangells95@gmail.com>
2017-01-22 19:53:05 -0200

    add rout GET /users/me x-auth headers to authenticate the user

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ var {ObjectID} = require("mongodb");
 var {mongoose} = require("./db/mongoose.js");
 var {Todo} = require("./models/todo.js");
 var {User} = require("./models/user.js");
+var {authenticate} = require('./middleware/authenticate');
 
 var app = express();
 
@@ -99,6 +100,11 @@ app.post("/users", (req, res) =>{
 		res.status(400).send(e);
 	});
 
+});
+
+
+app.get('/users/me', authenticate, (req, res) => {
+	res.send(req.user);
 });
 
 app.listen(3000, function(){
